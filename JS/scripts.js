2ffe8bd88b6b09fc6f98e89aedaafb5595e75438
ManuFlorenca! <manuelaflorenca22@gmail.com>
2024-12-12 00:19:48 -0300

    Falta gerar a senha de forma customizada

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,67 @@
-console.log("teste")
+// Seleção de Elementos
+
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatePasswordElement = document.querySelector("#generated-password");
+
+
+// Funções 
+
+const getLetterLowerCase = () => {
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+};
+
+const getLetterUpperCase = () => {
+    return (String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+};
+
+const getNumber = () => {
+
+    return Math.floor(Math.random() * 10).toString();
+
+};
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase,getNumber,getSymbol) => {
+    let password =""
+
+    const passwordLength = 10
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+
+    for(i =0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValues = generators [Math.floor(Math.random() * generators.length)]();
+
+        password += randomValues;
+
+        });
+    }
+
+    password = password.slice(0, passwordLength);
+    
+    generatePasswordElement.style.display = "block"
+    generatePasswordElement.querySelector("h4").innerText = password;
+
+};
+
+// Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+
+    generatePassword(  
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    );
+  
+});
